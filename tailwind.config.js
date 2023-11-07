283a71ec524877b7861582de731ca9f070f3d19e
Jam Maligad <jdmaligad@gmail.com>
2023-11-07 13:00:03 +0800

    Initialize custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orchardRed: '#b4162c'
+      }
+    },
     fontFamily: {
       helvetica: ['Helvetica', 'sans-serif'],
       rubik: ['Rubik', 'sans-serif']
