7d07e02738dce017945be3b125c3ed4d1207ed68
Jam Maligad <jdmaligad@gmail.com>
2023-11-09 21:42:24 +0800

    Create Section One Component

--- src/components/SectionOne.tsx
+++ src/components/SectionOne.tsx
@@ -1,7 +1,51 @@
+import imageOne from '../assets/component-01/Image-01.jpg'
+import imageTwo from '../assets/component-01/Image-02.jpg'
+import imageThree from '../assets/component-01/Image-03.jpg'
+
 const SectionOne = () => {
   return (
     <section id="section-ingredients">
-      <div>Section 1</div>
+      <div className="flex w-full space-x-6">
+        <div className="h-max w-full">
+          <img
+            src={imageOne}
+            className="h-full border border-neutral-900 shadow-lg"
+          />
+        </div>
+        <div className="w-full space-y-4">
+          <img src={imageTwo} className="border border-neutral-900 shadow-lg" />
+          <img
+            src={imageThree}
+            className="border border-neutral-900 shadow-lg"
+          />
+        </div>
+        <div className="w-full space-y-10">
+          <div className="w-full">
+            <h1 className="pb-4 font-open-sans-light text-2xl uppercase text-gray-300">
+              Answer your body&apos;s needs
+            </h1>
+            <hr />
+            <p className="pr-1 pt-8 font-open-sans-light font-light leading-relaxed text-gray-400">
+              The way ingredients are sourced affects the way we nourish our
+              bodies. Author Mark Schatzer believers our body naturally develops
+              an appetite for the foods and nutrients it needs to be healthy,
+              but that artificial flavourings are getting in the way. This can
+              be reversed by focusing on high-quality ingredients and being
+              mindful as your appetite guides you to consume according to your
+              body&apos;s needs.
+            </p>
+          </div>
+          <div className="w-full space-y-4">
+            <p className="font-open-sans-semibold text-sm uppercase text-orchard-red">
+              Be Mindful
+            </p>
+            <p className="font-open-sans-semibold leading-relaxed text-gray-300">
+              Sourcing local or organic food is a good way to start being more
+              mindful about what you&apos;re cooking and eating.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        orchardRed: '#b4162c'
+        'orchard-red': '#b4162c'
+      },
+      fontFamily: {
+        'open-sans': ['Open Sans', 'sans-serif'],
+        'open-sans-light': ['Open Sans Light', 'sans-serif'],
+        'open-sans-semibold': ['Open Sans Semibold', 'sans-serif'],
+        'open-sans-condensed': ['Open Sans Condensed', 'sans-serif'],
+        'open-sans-condensed-light': [
+          'Open Sans Condensed Light',
+          'sans-serif'
+        ],
+        'open-sans-semicondensed': ['Open Sans SemiCondensed', 'sans-serif'],
+        'open-sans-semicondensed-light': [
+          'Open Sans SemiCondensed Light',
+          'sans-serif'
+        ]
       }
-    },
-    fontFamily: {
-      helvetica: ['Helvetica', 'sans-serif'],
-      rubik: ['Rubik', 'sans-serif']
     }
   },
   plugins: []
